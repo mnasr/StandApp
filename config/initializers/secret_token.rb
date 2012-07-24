a0ee715914a3828bf7ab3245386dc5b93299c02b
Constantine Nicolaou <constantin.nicolaou@gmail.com>
2012-07-24 10:01:31 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StandApp::Application.config.secret_token = 'c0bfed6e97ae90cc8acd499f7240946b409701fcac797d8331b019d2066ae572b524e8774cc4d029c6ceb1bde096a8b73940a8da0f7a644bfe4809ddc5f8db97'
