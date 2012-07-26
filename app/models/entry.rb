5a9096ea8e8e62e297daae530ed117833f8c144d
mnasr <mnasr@monaqasat.com>
2012-07-26 11:58:54 +0300

    (#1310)unit test for the feature

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,4 @@
 class Entry < ActiveRecord::Base
   attr_accessible :category, :description, :ticket_id, :user_id
+  
 end
--- test/unit/helpers/entries_helper_test.rb
+++ test/unit/helpers/entries_helper_test.rb
@@ -1,4 +1,13 @@
 require 'test_helper'
 
 class EntriesHelperTest < ActionView::TestCase
+	test "format redmine ticket id into full url" do
+		assert_equal "http://dev.nuserv.com/issues/1234", format_ticket_id_to_url(1234)
+	end
+
+	test "ticket url is empty if redmine ticket_id is empty" do
+		assert_equal "", format_ticket_id_to_url
+		assert_equal "", format_ticket_id_to_url(nil)
+		assert_equal "", format_ticket_id_to_url("")
+	end
 end
