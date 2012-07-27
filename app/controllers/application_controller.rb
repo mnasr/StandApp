a2968493b8c39a253d03d3edc1c20e6e8adbe9da
Constantine Nicolaou <constantin.nicolaou@gmail.com>
2012-07-27 10:56:21 +0300

    Check scrum master expiration after user logs in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   
   before_filter :authenticate_user!
+  before_filter :check_valid_user
+  
+  def check_valid_user
+    if user_signed_in?
+      current_user.check_and_assign_if_date_expired
+    end
+  end
 end
