8a243d690286376027732645da5c4c90329a9a65
mnasr <mnasr@monaqasat.com>
2012-07-30 15:32:21 +0300

    (#1329)Functional Tests

--- test/functional/searches_controller_test.rb
+++ test/functional/searches_controller_test.rb
@@ -6,13 +6,15 @@ class SearchesControllerTest < ActionController::TestCase
     sign_in @user
   end
 
-  test "search should return the convenient keywords" do
+  test "search should return results matching a keyword" do
     post :create, :search => "nasr@monaqasat.com"
-    assert assigns(:results)
+    @results = assigns(:results)
+    assert @results.present?
   end
-    test "search result should not be empty" do
+
+  test "search result should not be empty" do
   	post :create, :search => ""
-    assert_equal 0, results.size
-    
-    end
-end 
+    assert_response :success
+    assert_template "search"
+  end
+end
