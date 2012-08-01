4e18b47f7c4b82ac1d6ea7a5afb43f46f479ce45
ghinaajour <gajour@monaqasat.com>
2012-08-01 16:57:34 +0300

    Adding buttons,show search results in a table and modify the navigation bar

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ StandApp::Application.routes.draw do
 
   resources :users
 
+  match '/entries/:id/edit' => 'entries#edit', :via => :post
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
