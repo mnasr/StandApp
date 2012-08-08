7b476e3dede4a828d3e6921a33382774599c53d9
Constantine Nicolaou <constantin.nicolaou@gmail.com>
2012-08-08 09:20:47 +0300

    Users should always provide password_confirmation when updating the password

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,11 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-
-
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :fullname, :admin
+
   validates :password, confirmation: true
+  validates :password_confirmation, presence: true
 
   validates :fullname, uniqueness: true 
   validates :fullname, presence: true
