98aedd9b86495e0ae0d87af7f86633e6347b4782
ghinaajour <gajour@monaqasat.com>
2012-08-09 17:09:07 +0300

    Deleted files

--- app/models/summary.rb
+++
@@ -1,3 +0,0 @@
-class Summary < ActiveRecord::Base
-  # attr_accessible :title, :body
-end
--- db/migrate/20120808130534_create_summaries.rb
+++
@@ -1,8 +0,0 @@
-class CreateSummaries < ActiveRecord::Migration
-  def change
-    create_table :summaries do |t|
-
-      t.timestamps
-    end
-  end
-end
