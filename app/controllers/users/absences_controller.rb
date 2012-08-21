aa2082d751b677390dada3d075b141f73a5895f2
mnasr <mnasr@monaqasat.com>
2012-08-21 09:59:43 +0300

    Add pagination to both absences controllers

--- app/controllers/users/absences_controller.rb
+++ app/controllers/users/absences_controller.rb
@@ -3,7 +3,8 @@ class Users::AbsencesController < ApplicationController
   # GET /absences.json
   def index
     @absences = Absence.where(user_id: params[:user_id])
-
+    @absences = Absence.paginate(:page => params[:page], :order => "created_at DESC")
+    @title = "Absences"
     respond_to do |format|
       format.html { render :file => "absences/index"}
       format.json { render json: @absences }
@@ -14,7 +15,7 @@ class Users::AbsencesController < ApplicationController
   # GET /absences/1.json
   def show
     @absence = Absence.where(id: params[:id], user_id: params[:user_id])
-        
+    @title = "User #{@absence.user_id} Absence"   
     respond_to do |format|
       format.html { render :file => "absences/show"}
       format.json { render json: @absence }
