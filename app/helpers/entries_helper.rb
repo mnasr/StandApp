b01b2d2064d0d94fba4ee9aa5bbad4f0c0ea0f5d
ghinaajour <gajour@monaqasat.com>
2012-09-14 13:00:37 +0300

    Adding two methods from linking to the category and ticket id inside the description and removing the linkify method from the entry helper

--- app/helpers/entries_helper.rb
+++ app/helpers/entries_helper.rb
@@ -11,12 +11,4 @@ module EntriesHelper
   def all_users
     @users = User.all
   end
-
-  def linkify(body)
-    links = body.scan(/\#\d+/).map{|id| id.gsub(/#/,'')}
-    ticket_url = Settings.redmine_url
-    populated_links = ""
-    links.each {|link| populated_links << link_to(link, "#{ticket_url}#{link}")}
-    populated_links.html_safe
-  end
 end
