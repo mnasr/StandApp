78458435c5c02db3c5b2c8e8ea39e0818813732c
mnasr <mnasr@monaqasat.com>
2012-09-14 17:35:28 +0300

    Adding placeholder to the default textarea form of an entry

---
+++ app/assets/javascripts/markdown_placeholder.js
@@ -0,0 +1,3 @@
+var placeholder_text = "### Yesterday: ###\n\n* \n* \n* \n\n### Today: ###\n\n* \n* \n* \n\n\n### Roadblocks: (please write none if there is nothing) ###\n\n* \n* \n* \n";
+
+$('textarea#editor').attr('value', placeholder_text);
