db6836c7f0f8b2ade19b2b0901019ce7ceff39e0
Constantine Nicolaou <constantin.nicolaou@gmail.com>
2012-09-17 17:42:22 +0300

    Drop search on user_id because it's a rare case to search using a user_id

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -7,7 +7,7 @@ class SearchesController < ApplicationController
       search_keywords = "%#{@keywords}%"
       @results[:user] = User.where("email LIKE ? OR fullname LIKE ?", search_keywords, search_keywords).all.flatten.compact
       @results[:entry] = Entry.where("description LIKE ?", search_keywords).all.flatten.compact
-      @results[:absence] = Absence.where("description LIKE ? OR user_id LIKE ?", search_keywords, search_keywords).all.flatten.compact
+      @results[:absence] = Absence.where("description LIKE ?", search_keywords).all.flatten.compact
     end
 
     render :action => :show, :locals => {:results => @results, :keywords => @keywords}
